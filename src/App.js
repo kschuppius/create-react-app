df97e5a333dfae3e663a262484943c29e398755e
kschuppius <kschuppius@gmail.com>
2023-09-26 12:39:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <h1>Welcome to my card</h1>
+        <img src="https://marketplace.canva.com/EAE3lxebXtk/1/0/1600w/canva-home%2C-real-estate%2C-home-design-logo-eaGl0uXNSMo.jpg" alt="House Image" />
+        <div className="paragraph">
+          <p>We provide quality Real Estate over here at Pod 2
+          Please Contact us on Canvas for further Inquires!</p>
+        </div>
+        <div class="btn-wrapper">
+ <a href="https://hax.psu.edu">
+   <button>Details</button> </a> </div>
+      </div>
+    );
+  }
 
 export default App;
